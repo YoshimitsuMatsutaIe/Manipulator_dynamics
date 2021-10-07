73f67258dd86dd1ee7eb1a69f9db4e4b2edbbe5f
yoshimitsumatsutaIe <sbb03104@edu.osakafu-u.ac.jp>
2021-10-07 15:25:55 +0900

    gakou

--- py/kinematics/rmp_simulation.py
+++ py/kinematics/rmp_simulation.py
@@ -39,12 +39,12 @@ class FrameData:
     
     
     def _get_cpoints_position(self, arm):
-        self.cpoints_potisions = []
+        self.cpoints_potisions_list = []
         for cpoints in arm.cpoints_x:
             _cs = PositinData()
             for c in cpoints:
                 _cs.add(c)
-            self.cpoints_potisions.append(_cs)
+            self.cpoints_potisions_list.append(_cs)
         return
 
 
@@ -121,12 +121,17 @@ class Simulator:
             pulled_M_all = []
             
             for i in range(8):
-                for x, dx, J in zip(arm.cpoints_x[i], arm.cpoints_dx[i], arm.Jos_cpoints[i]):
+                for x, dx, J, dJ in zip(
+                    arm.cpoints_x[i],
+                    arm.cpoints_dx[i],
+                    arm.Jos_cpoints[i],
+                    arm.Jos_cpoints_diff_by_t[i],
+                ):
                     a = rmp.a_obs(x, dx, self.obs)
                     M = rmp.metric_obs(x, dx, self.obs, a)
                     f = M @ a
                     
-                    _pulled_f = J.T @ f
+                    _pulled_f = J.T @ (f - M @ dJ @ dq)
                     _pulled_M = J.T @ M @ J
                     
                     pulled_f_all.append(_pulled_f)
@@ -137,7 +142,7 @@ class Simulator:
                         M = rmp.metric_attract(x, dx, self.gl_goal, a)
                         f = M @ a
                         
-                        _pulled_f = J.T @ f
+                        _pulled_f = J.T @ (f - M @ dJ @ dq)
                         _pulled_M = J.T @ M @ J
                         
                         pulled_f_all.append(_pulled_f)
@@ -181,8 +186,168 @@ class Simulator:
         return
 
 
-    def plot(self,):
+    def plot_animation(self,):
         """グラフ作成"""
+        
+        start = time.time()
+        print("plot実行中...")
+        
+        # アニメーション
+        fig_ani = plt.figure()
+        ax = fig_ani.add_subplot(projection = '3d')
+        ax.grid(True)
+        ax.set_xlabel('X[m]')
+        ax.set_ylabel('Y[m]')
+        ax.set_zlabel('Z[m]')
+
+        # ## 三軸のスケールを揃える
+        # max_x = 1.0
+        # min_x = -1.0
+        # max_y = 0.2
+        # min_y = -1.0
+        # max_z = 2.0
+        # min_z = 0.0
+        
+        # max_range = np.array([
+        #     max_x - min_x,
+        #     max_y - min_y,
+        #     max_z - min_z
+        #     ]).max() * 0.5
+        # mid_x = (max_x + min_x) * 0.5
+        # mid_y = (max_y + min_y) * 0.5
+        # mid_z = (max_z + min_z) * 0.5
+        # ax.set_xlim(mid_x - max_range, mid_x + max_range)
+        # ax.set_ylim(mid_y - max_range, mid_y + max_range)
+        # ax.set_zlim(mid_z - max_range, mid_z + max_range)
+
+
+
+
+        # 目標点
+        ax.scatter(
+            self.gl_goal[0, 0], self.gl_goal[1, 0], self.gl_goal[2, 0],
+            s = 100, label = 'goal point', marker = '*', color = '#ff7f00', 
+            alpha = 1, linewidths = 1.5, edgecolors = 'red')
+        ax.scatter(
+            self.obs[0, 0], self.obs[1, 0], self.obs[2, 0],
+            s = 100, label = 'obstacle point', marker = '+', color = 'k', 
+            alpha = 1)
+
+
+        # 初期値
+        d = self.data.data[0]
+
+        # ジョイント位置
+        bodys = []
+        bodys.append(
+            ax.plot(
+                d.joint_positions_list.x,
+                d.joint_positions_list.y,
+                d.joint_positions_list.z,
+                "o-", color = "blue"
+            )[0]
+        )
+
+        # 制御点
+        cpoints_all = []
+        for p in d.cpoints_potisions_list:
+            cpoints_all.append(
+                ax.scatter(
+                    p.x, p.y, p.z,
+                )
+            )
+
+
+        # グリッパー（エンドエフェクター）軌跡
+        gl = []
+        gl.append(
+            ax.plot(
+                self.data.ee.x[0], self.data.ee.y[0], self.data.ee.z[0],
+                "-", label = "gl", color = "#ff7f00",
+            )[0]
+        )
+
+        # 時刻表示
+        timeani = [ax.text(0.8, 0.2, 0.01, "time = 0.0 [s]", size = 10)]
+        time_template = 'time = %s [s]'
+
+        # # 結果表示
+        # ax.text(0.8, 0.3, 0.01, result[0], color = "r", size = 14)
+
+        ax.set_box_aspect((1,1,1))
+
+        def _update(i):
+            """アニメーションの関数"""
+            #i = i + 1
+            
+            d = self.data.data[i]
+            
+            
+            item1 = bodys.pop(0)
+            ax.lines.remove(item1)
+            bodys.append(
+                ax.plot(
+                    d.joint_positions_list.x,
+                    d.joint_positions_list.y,
+                    d.joint_positions_list.z,
+                    "o-", color = "blue"
+                )[0]
+            )
+            
+            
+            # for i, p in enumerate(d.cpoints_potisions_list):
+            #     item = cpoints_all[i].pop(0)
+            #     ax.lines.remove(item)
+            #     cpoints_all[i].append(
+            #         ax.scatter(
+            #             p.x, p.y, p.z,
+            #         )[0]
+            #     )
+            
+            item2 = gl.pop(0)
+            ax.lines.remove(item2)
+            gl.append(ax.plot(
+                self.data.ee.x[1:i],
+                self.data.ee.y[1:i],
+                self.data.ee.z[1:i],
+                "-", color = "#ff7f00")[0]
+            )
+            
+            # 時刻表示
+            timeani.pop().remove()
+            timeani_, = [
+                ax.text(
+                    0.8, 0.12, 0.01,
+                    time_template % (i * self.TIME_INTERVAL), size = 10
+                )
+            ]
+            timeani.append(timeani_)
+            
+            ax.set_box_aspect((1,1,1))
+            
+            return
+
+        ani = anm.FuncAnimation(
+            fig = fig_ani,
+            func = _update,
+            frames=len(self.data.data),
+            interval = self.TIME_INTERVAL * 0.001
+        )
+
+        #ani.save("hoge.gif", fps=1/self.TIME_INTERVAL, writer='pillow')
+        
+        
+        print("plot実行終了")
+        print("実行時間 = ", time.time() - start)
+        
+        plt.show()
+        
+        
+        return
+
+
+    def plot_animation_2(self,):
+        """グラフ作成（遅いかも）"""
         
         start = time.time()
         print("plot実行中...")
@@ -216,96 +381,66 @@ class Simulator:
         ax.set_zlim(mid_z - max_range, mid_z + max_range)
 
 
+        # 時刻表示
+        time_template = 'time = %s [s]'
+        
 
+        ax.set_box_aspect((1,1,1))
 
-        # 目標点
-        ax.scatter(
-            self.gl_goal[0, 0], self.gl_goal[1, 0], self.gl_goal[2, 0],
-            s = 100, label = 'goal point', marker = '*', color = '#ff7f00', 
-            alpha = 1, linewidths = 1.5, edgecolors = 'red')
-        ax.scatter(
-            self.obs[0, 0], self.obs[1, 0], self.obs[2, 0],
-            s = 100, label = 'obstacle point', marker = '+', color = 'k', 
-            alpha = 1)
-
-
-        # 初期値
-        d = self.data.data[0]
-
-        # アーム全体
-        bodys = []
-        bodys.append(
+        def _update(i):
+            """アニメーションの関数"""
+            ax.cla()  # 遅いかも
+            
+            
+            # 目標点
+            ax.scatter(
+                self.gl_goal[0, 0], self.gl_goal[1, 0], self.gl_goal[2, 0],
+                s = 100, label = 'goal point', marker = '*', color = '#ff7f00', 
+                alpha = 1, linewidths = 1.5, edgecolors = 'red')
+            # 障害物点
+            ax.scatter(
+                self.obs[0, 0], self.obs[1, 0], self.obs[2, 0],
+                s = 100, label = 'obstacle point', marker = '+', color = 'k', 
+                alpha = 1)
+            
+            d = self.data.data[i]
+            
+            # ジョイント位置
             ax.plot(
                 d.joint_positions_list.x,
                 d.joint_positions_list.y,
                 d.joint_positions_list.z,
                 "o-", color = "blue"
-            )[0]
-        )
-
-        # グリッパー（エンドエフェクター）軌跡
-        gl = []
-        gl.append(
-            ax.plot(
-            self.data.ee.x[0], self.data.ee.y[0], self.data.ee.z[0],
-            "-", label = "gl", color = "#ff7f00"
-            )[0]
-        )
-
-        # 時刻表示
-        timeani = [ax.text(0.8, 0.2, 0.01, "time = 0.0 [s]", size = 10)]
-        time_template = 'time = %s [s]'
-
-        # # 結果表示
-        # ax.text(0.8, 0.3, 0.01, result[0], color = "r", size = 14)
-
-        #ax.set_box_aspect((1,1,1))
-
-        def _update(i):
-            """アニメーションの関数"""
-            #i = i + 1
-            
-            d = self.data.data[i]
-            
-            
-            item1 = bodys.pop(0)
-            ax.lines.remove(item1)
-            bodys.append(
-                ax.plot(
-                    d.joint_positions_list.x,
-                    d.joint_positions_list.y,
-                    d.joint_positions_list.z,
-                    "o-", color = "blue"
-                )[0]
             )
             
-            item2 = gl.pop(0)
-            ax.lines.remove(item2)
-            gl.append(ax.plot(
-                self.data.ee.x[1:i],
-                self.data.ee.y[1:i],
-                self.data.ee.z[1:i],
-                "-", color = "#ff7f00")[0]
+            # 制御点
+            for p in d.cpoints_potisions_list:
+                ax.scatter(
+                    p.x, p.y, p.z,
+                )
+            
+            # グリッパー
+            ax.plot(
+                self.data.ee.x[0:i],
+                self.data.ee.y[0:i],
+                self.data.ee.z[0:i],
+                "-", color = "#ff7f00"
             )
             
             # 時刻表示
-            timeani.pop().remove()
-            timeani_, = [
-                ax.text(
-                    0.8, 0.12, 0.01,
-                    time_template % (i * self.TIME_INTERVAL), size = 10
-                )
-            ]
-            timeani.append(timeani_)
+            ax.text(
+                0.8, 0.12, 0.01,
+                time_template % (i * self.TIME_INTERVAL), size = 10
+            )
             
-            #ax.set_box_aspect((1,1,1))
-            
+            ax.set_box_aspect((1,1,1))
+
             return
 
         ani = anm.FuncAnimation(
             fig = fig_ani,
             func = _update,
-            frames=len(self.data.data),
+            frames = len(self.data.data),
             interval = self.TIME_INTERVAL * 0.001
         )
 
@@ -326,7 +461,8 @@ class Simulator:
 def main():
     simu = Simulator()
     simu.run_simulation()
-    simu.plot()
+    # simu.plot_animation()
+    simu.plot_animation_2()
 
 
 
